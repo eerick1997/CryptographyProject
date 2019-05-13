d166d749a68646e720a725148f10bbbd0d21ed61
Liliana <aideegf14@gmail.com>
2019-05-13 00:38:27 -0500

    Interfaces de usuario modificación

---
+++ app/src/main/java/com/example/digitalportrait/Home.java
@@ -0,0 +1,20 @@
+package com.example.digitalportrait;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewOutlineProvider;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Home extends Fragment {
+    @NonNull
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView= inflater.inflate(R.layout.frag_home,container,false);
+        return rootView;
+    }
+}
--- app/src/main/java/com/example/digitalportrait/MainActivityUser.java
+++ app/src/main/java/com/example/digitalportrait/MainActivityUser.java
@@ -14,6 +14,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -84,21 +85,24 @@ public class MainActivityUser extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
+        Fragment fragment=null;
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+            fragment=new Home();
+        }
+        else if(id == R.id.nav_send) {
+            fragment=new Send();
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_receive) {
+            fragment=new Receive();
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        }else if (id == R.id.nav_gallery) {
+            fragment=new Gallery();
+        }
+        else if (id == R.id.nav_power) {
+            fragment=new EndSession();
 
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
